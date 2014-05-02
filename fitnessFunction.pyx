8c6f4c4a0f93302e2eac17eaac4aabbe4e0161f2
Aaron Scott Pope <asphwb@mst.edu>
2014-05-02 17:34:46 -0500

    Added argument parsing for output directory, run SGA.py with -h for details. Added min and max average eccentricity fitness evaluations.

--- fitnessFunction.pyx
+++ fitnessFunction.pyx
@@ -38,9 +38,42 @@ def minMetisEval(state,partitions=2):
         return -50*float(edgecuts)+G.number_of_edges()#nx.radius(G)
     return -99999999999999999
 
+def minEccenEval(state):
+    G = nx.Graph()
+    for node in xrange(len(state.nodeList)):
+        G.add_node(node)
+        for edge in state.nodeList[node]:
+            G.add_edge(node,edge)
+    if nx.is_connected(G):
+        avgEccen = 0.0
+        nodeEccentricies = nx.eccentricity(G)
+        for node in nodeEccentricies:
+            avgEccen += nodeEccentricies[node]
+        avgEccen /= len(state.nodeList)
+        return -50 * avgEccen - G.number_of_edges()
+    return -99999999999999999
+
+def maxEccenEval(state):
+    G = nx.Graph()
+    for node in xrange(len(state.nodeList)):
+        G.add_node(node)
+        for edge in state.nodeList[node]:
+            G.add_edge(node,edge)
+    if nx.is_connected(G):
+        avgEccen = 0.0
+        nodeEccentricies = nx.eccentricity(G)
+        for node in nodeEccentricies:
+            avgEccen += nodeEccentricies[node]
+        avgEccen /= len(state.nodeList)
+        return 50 * avgEccen - G.number_of_edges()
+    return -99999999999999999
+
+
 
 funcs = {}
 funcs['minMetis'] = minMetisEval
 funcs['maxMetis'] = maxMetisEval
+funcs['minEccen'] = minEccenEval
+funcs['maxEccen'] = maxEccenEval
 
 
